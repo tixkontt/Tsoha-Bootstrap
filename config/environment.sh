6837d0f1ef987b579776b5203d0e9395902a8911
Konttinen Timo V <tixkontt@users2017.cs.helsinki.fi>
2017-11-03 22:24:25 +0200

    Sovellus alustettu ja versionhallinta otettu käyttöön 3.11. klo 22.24

--- config/environment.sh
+++ config/environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # korvaa tämä omalla käyttäjätunnuksellasi
-USERNAME="kalleilv"
+USERNAME="tixkontt"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="tsoha"
+PROJECT_FOLDER="SuomalaisetMaailmalla"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
