945f603bcc9f3a5b15cbc8f64e76379dec8140b2
Konttinen Timo V <tixkontt@users2017.cs.helsinki.fi>
2017-11-05 17:42:25 +0200

    Korjattu kansion nimen kirjoitusvirhe

--- config/environment.sh
+++ config/environment.sh
@@ -3,5 +3,5 @@
 # korvaa tämä omalla käyttäjätunnuksellasi
 USERNAME="tixkontt"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER="SuomalaisetMaailmalla"
+PROJECT_FOLDER="Tietokantalabra"
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
