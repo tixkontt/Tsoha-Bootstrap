c0119c94389ff73e2964428ffae4ca79724ca002
Timo Konttinen <timo.konttinen@helsinki.fi>
2017-11-12 00:23:40 +0200

    Netbeans ei siirrä tietoja.

--- config/environment.sh
+++ config/environment.sh
@@ -2,12 +2,12 @@
 
 # korvaa tämä omalla käyttäjätunnuksellasi
 <<<<<<< HEAD
-USERNAME=tixkontt
+USERNAME="tixkontt"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER=Tietokantalabra
+PROJECT_FOLDER="Tietokantalabra"
 =======
-USERNAME=tixkontt
+USERNAME="tixkontt"
 # korvaa tämä haluamallasi kansion nimellä
-PROJECT_FOLDER=Tietokantalabra
+PROJECT_FOLDER="Tietokantalabra"
 >>>>>>> 6c846d947da4a7cc42d433664fa41f2bddac96ed
 # sovelluksesi tulee sijaitsemaan osoitteessa USERNAME.users.cs.helsinki.fi/PROJECT_FOLDER
