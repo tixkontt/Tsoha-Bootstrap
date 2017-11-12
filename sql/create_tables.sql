4fd6266518b9b252cdfed96895469ce92a8af1a6
Timo Konttinen <timo.konttinen@helsinki.fi>
2017-11-12 13:16:19 +0200

    korjattu typoja

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,32 +1,32 @@
 CREATE TABLE henkilo(
-id integer SERIAL PRIMARY KEY, 
-firstNames varchar(50) NOT null,
-familyname varchar(50) NOT null,
-dateOfBirth varchar(10) NOT null,
-gender varchar(12),
-nationality varchar(50) NOT null,
-mobilePhone varchar(25),
-email varchar(30)
-);
+	id SERIAL PRIMARY KEY, 
+	firstNames varchar(50) NOT null,
+	familyname varchar(50) NOT null,
+	dateOfBirth varchar(10) NOT null,
+	gender varchar(12),
+	nationality varchar(50) NOT null,
+	mobilePhone varchar(25),
+	email varchar(30)
+	);
 
 CREATE TABLE matkakohde(
-id integer SERIAL PRIMARY KEY, 
-country varchar(50) NOT NUll,
-arrivalDate date NOT null,
-departureDate date NOT null,
-address varchar(60),
-postCode varchar(10),
-city varchar(30)
-);
+	id SERIAL PRIMARY KEY, 
+	country varchar(50) NOT NUll,
+	arrivalDate date NOT null,
+	departureDate date NOT null,
+	address varchar(60),
+	postCode varchar(10),
+	city varchar(30)
+	);
 
-CREATE TABLE maa(
-id integer SERIAL PRIMARY KEY, 
-country varchar(50) NOT NULL
+--CREATE TABLE maa(
+--id integer SERIAL PRIMARY KEY, 
+--country varchar(50) NOT NULL
 
-);
+--);
 
-CREATE TABLE JOINTABLE(
-id integer SERIAL PRIMARY KEY,
-FOREIGN KEY (countryKey) REFERENCES maa (id),
-FOREIGN KEY (henkiloId) REFERENCES henkilo (id)
-);
+--CREATE TABLE JOINTABLE(
+--id integer SERIAL PRIMARY KEY,
+--FOREIGN KEY (countryKey) REFERENCES maa (id),
+--FOREIGN KEY (henkiloId) REFERENCES henkilo (id)
+--);
