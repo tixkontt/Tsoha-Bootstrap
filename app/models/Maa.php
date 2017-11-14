ca6cda610d01d7b442cfc7210f6ab80f0d1f799b
Timo Konttinen <timo.konttinen@helsinki.fi>
2017-11-14 16:24:46 +0200

    uusi yritys...

--- app/models/Maa.php
+++ app/models/Maa.php
@@ -1,6 +1,6 @@
 <?php
 
-class maalista extends BaseModel {
+class Maa extends BaseModel {
 
 //attribuutit
     public $id, $country;
@@ -8,10 +8,18 @@ class maalista extends BaseModel {
     public function __construct($attributes) {
         parent::__construct($attributes);
     }
+    
+    public static function maat(){
+     $query=DB::connection()()-->prepare(copy * From /home/tixkontt/Tietokantalabra/Tsoha-Bootstrap/countries.txt DELIMITER as '\n'; )   
+        
+        
+    }
 
 }
 
-/*
+/* COPY master FROM 'D:\demo.csv'  DELIMITER AS ',';
+ * 
+ * 
 CREATE TABLE maa(
         id SERIAL PRIMARY KEY, 
         country varchar(50) NOT NULL
