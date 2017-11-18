8bee09769dfabe907a06889be4eef776b38b6c9e
Timo Konttinen <timo.konttinen@helsinki.fi>
2017-11-18 13:06:13 +0200

    Ei toimi!

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,1 @@
-INSERT INTO henkilo (ID, firstnames, familyname,dateofbirth, nationality) VALUES (1,'aku','Ankka','2000-1-1','Suomi');
+INSERT INTO  henkilo VALUES (1,'aku','Ankka','2000-1-1','Suomi');
